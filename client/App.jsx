11a97198f9fded5b083f43f102e0a041eb5378a1
George German <georgegerman.752@gmail.com>
2024-10-09 19:50:24 -0700

    1st commit

---
+++ client/App.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                This is a test!
+            </h1>
+        </div>
+    );
+}
+
+module.exports = App;
---
+++ index.js
@@ -0,0 +1,17 @@
+const React = require('react');
+// import ReactDOM from 'react-dom/client'; // Correct import for React 18 and higher
+const App = require('./client/App.jsx'); // Make sure the path and casing are correct
+// const ReactDOM = require('react-dom/client')
+const { createRoot } = require('react-dom/client')
+// Create the root element
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component
+// root.render(
+//     <App />
+// );
+
+const container = document.getElementById('root'); // Get the root element
+console.log(container);
+const root = createRoot(container); // Create a root
+root.render(<App />); // Render your App inside the root
