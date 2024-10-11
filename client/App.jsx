b04e5af3833c9c7538265c574d0934b06b964b62
Anthony Noyola <noyola99@gmail.com>
2024-10-10 19:48:11 -0700

    Added general skeleton for Frontend, pseudocoded logic for Goallist
    Co-authored-by: Jacob jlalexander102@gmail.com

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,26 @@
 const React = require('react');
+const GoalList = require('./components/GoalList');
+const Graph = require('./components/Graph');
+const GoalCreator = require('./components/GoalCreator');
 
 const App = () => {
-    return (
-        <div>
-            <h1>
-                This is a test!
-            </h1>
-        </div>
-    );
-}
+  // Function for creating a goal and sending a request to the back end
+    
+  // Styled components?
+
+
+  return (
+    <div>
+      <h1>This is a test!</h1>
+      <div>
+        <GoalList></GoalList>
+      </div>
+      <div>
+        {/* <Graph></Graph>
+        <GoalCreator></GoalCreator> */}
+      </div>
+    </div>
+  );
+};
 
 module.exports = App;
