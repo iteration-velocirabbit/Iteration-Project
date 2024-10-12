6a086d12edab5dcdd779df11a9e1111eac27e27a
Christopher Matzen <christophermatzen@Christophers-MacBook-Pro.local>
2024-10-12 13:53:27 -0700

    Server updates (/fetchgoals)

--- routers/api.js
+++ routers/api.js
@@ -2,9 +2,25 @@ const express = require('express');
 const router = express.Router();
 const app = require('../server.js');
 const goalsController = require('../controllers/goalsController.js');
+const userController = require('../controllers/userController.js');
 
-router.get('/', goalsController.getAllData, (req, res) => {
-    res.status(200).json(res.locals.test)
+// fetching goals after login
+router.get('/', goalsController.getAllGoals, (req, res) => {
+    res.status(200).json(res.locals.goals);
+})
+
+// for login and querying users table in DB
+router.get('/users', userController.getAllUsers, (req, res) => {
+    res.status(200).json(res.locals.users);
+})
+
+// for logging in (POST request)
+router.post('/login', userController.login, (req, res) => {
+    res.status(200).json({ success:true, loggedInUser: res.locals.login });
+})
+
+router.get('/fetchgoal', goalsController.getUserGoals, (req, res) => {
+    res.status(200).json(res.locals.userGoal);
 })
 
 module.exports = router;
