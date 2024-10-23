6f1c75153c21488f7941aeb0a4f8a96fff57635f
SungGuk Jung <sunggukjung@sungguk-mbp.mynetworksettings.com>
2024-10-22 19:27:27 -0700

    updates

--- models/goalAppModels.js
+++ models/goalAppModels.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 // store in .env file later for security (?)
-const PG_URI = 'postgresql://postgres.prqijpejzhcknxrjhmwp:i304vBTIupL1b9gR@aws-0-us-west-1.pooler.supabase.com:6543/postgres';
+const PG_URI = 'postgresql://postgres.fjlwodtivsuankqmurkh:o1v3yLcMJiWHdbnM@aws-0-us-west-1.pooler.supabase.com:6543/postgres';
 
 // create pool w/ above connection string
 const pool = new Pool({
