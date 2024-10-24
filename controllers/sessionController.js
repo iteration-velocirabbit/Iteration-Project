39c13bd323939100012b50094fa0555bde8163ce
SungGuk Jung <sunggukjung@sungguk-mbp.mynetworksettings.com>
2024-10-23 19:41:38 -0700

    updates on sessions

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -6,21 +6,21 @@ const sessionController ={};
 sessionController.startSession = async(req,res,next)=>{
 
 try{
-const{username, id} = res.locals.user;
-let queryText = 'SELECT * FROM user WHERE username = $1';
+const{username, id} = res.locals.login;
+let queryText = 'SELECT * FROM sessions WHERE cookie_id = $1';
 
 
 let results = await db.query(queryText, [username])
-
+//console.log( 'RESULTS:', results)
 if(results.rows.length===0){
 let insertText = 'INSERT into sessions (cookie_id, user_id) VALUES($1, $2) RETURNING *';
 
 const newSession = await db.query(insertText, [username, id]);
-console.log('new session', newSession)
+console.log('new session', newSession.rows[0])
 }
 else{
     const existingSession = results.rows[0];
-    console.log('current session', existingSession);
+    console.log('current session', existingSession.rows[0]);
 }
 
 return next();
